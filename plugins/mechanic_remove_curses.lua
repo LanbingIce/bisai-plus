f6ac9a8799650c1a4fb94db32faf96fee0b3ea17
LanbingIce <895446745@qq.com>
2026-03-13 12:44:32 +0800

    feat: 如果是第二章节及之后，禁止XL诅咒生成

--- plugins/mechanic_remove_curses.lua
+++ plugins/mechanic_remove_curses.lua
@@ -1,7 +1,13 @@
 local function OnCurseEval(_, curses)
-	-- 移除迷路诅咒
+	-- 移除迷路诅咒和黑暗诅咒
 	curses = curses & ~LevelCurse.CURSE_OF_DARKNESS
 	curses = curses & ~LevelCurse.CURSE_OF_THE_LOST
+	local level = Game():GetLevel()
+	local stage = level:GetStage()
+	-- 如果是第二章节及之后，移除XL诅咒
+	if stage >= LevelStage.STAGE2_1 then
+		curses = curses & ~LevelCurse.CURSE_OF_LABYRINTH
+	end
 	return curses
 end
 
