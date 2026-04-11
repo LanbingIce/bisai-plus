c445f0980d325b3d706240e4c79d0d9d3b812f49
LanbingIce <895446745@qq.com>
2026-04-11 16:48:03 +0800

    fix: 修复大撒旦终点每次使用小红罐都会多一对钥匙碎片的问题

--- plugins/route_mega_satan_key.lua
+++ plugins/route_mega_satan_key.lua
@@ -1,3 +1,9 @@
+local EsauJrUsedFrame = 0
+
+local function OnUseEsauJr()
+	EsauJrUsedFrame = Game():GetFrameCount()
+end
+
 local function OnPlayerUpdate(_, player)
 	-- 确保是大撒旦终点
 	if BISAI_PLUS.Data.Save.Goal ~= BISAI_PLUS.Shared.Goal.MEGA_SATAN then
@@ -14,6 +20,12 @@ local function OnPlayerUpdate(_, player)
 		return
 	end
 
+	-- 确保这一帧没有使用过小红罐
+	-- 因为使用小红罐的那一帧，判断角色身上是否有钥匙碎片总会返回false，原因未知
+	if EsauJrUsedFrame == Game():GetFrameCount() then
+		return
+	end
+
 	-- 为角色添加钥匙碎片1
 	if not player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1) then
 		player:AddCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1)
@@ -26,3 +38,4 @@ local function OnPlayerUpdate(_, player)
 end
 
 BISAI_PLUS:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, OnPlayerUpdate)
+BISAI_PLUS:AddCallback(ModCallbacks.MC_USE_ITEM, OnUseEsauJr, CollectibleType.COLLECTIBLE_ESAU_JR)
