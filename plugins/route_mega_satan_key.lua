2dca47e103f3986f6ae82bc8dc0b63e013858e7c
LanbingIce <895446745@qq.com>
2026-04-11 16:42:22 +0800

    refactor: 使用卫语句重构大撒旦终点给钥匙碎片的插件的逻辑

--- plugins/route_mega_satan_key.lua
+++ plugins/route_mega_satan_key.lua
@@ -1,18 +1,27 @@
 local function OnPlayerUpdate(_, player)
+	-- 确保是大撒旦终点
+	if BISAI_PLUS.Data.Save.Goal ~= BISAI_PLUS.Shared.Goal.MEGA_SATAN then
+		return
+	end
+
+	-- 确保是RUNNING状态
+	if BISAI_PLUS.Data.Save.State ~= BISAI_PLUS.Shared.State.RUNNING then
+		return
+	end
+
+	-- 确保是真正的角色，而不是小罗饰品或者店长稻草人之类的
 	if not BISAI_PLUS.GameUtils.IsRealPlayer(player) then
 		return
 	end
-	-- 为大撒旦终点添加钥匙
-	if
-		BISAI_PLUS.Data.Save.State == BISAI_PLUS.Shared.State.RUNNING
-		and BISAI_PLUS.Data.Save.Goal == BISAI_PLUS.Shared.Goal.MEGA_SATAN
-	then
-		if not player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1) then
-			player:AddCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1)
-		end
-		if not player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2) then
-			player:AddCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2)
-		end
+
+	-- 为角色添加钥匙碎片1
+	if not player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1) then
+		player:AddCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1)
+	end
+
+	-- 为角色添加钥匙碎片2
+	if not player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2) then
+		player:AddCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2)
 	end
 end
 
